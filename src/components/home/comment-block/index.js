bbf9fe432bfe7f0bc891e138ae5dbbb6590160b5
MkhitaryanMartin <1992Martin@mail.ru>
2024-04-14 21:45:04 +0400

    slider

--- src/components/home/comment-block/index.js
+++ src/components/home/comment-block/index.js
@@ -1,73 +1,9 @@
-import React, { useState, useRef } from "react";
-import { comments } from "../../../assets/data";
-import CommentCard from "../../comment-card";
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-import { IconButton } from "@mui/material";
+import React  from "react";
+import Slider from "../../swiper";
 import "./style.scss";
 
 export default function CommentBlock() {
-    const categoryListRef = useRef(null);
-    const [isDown, setIsDown] = useState(false);
-    const [startX, setStartX] = useState(0);
-    const [scrollLeft, setScrollLeft] = useState(0);
 
-    const handleMouseDown = (e) => {
-        setIsDown(true);
-        setStartX(e.pageX - categoryListRef.current.offsetLeft);
-        setScrollLeft(categoryListRef.current.scrollLeft);
-    };
 
-    const handleMouseUp = () => {
-        setIsDown(false);
-    };
-
-    const handleMouseMove = (e) => {
-        if (!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - categoryListRef.current.offsetLeft;
-        const walk = (x - startX) * 2;
-        categoryListRef.current.scrollLeft = scrollLeft - walk;
-    };
-
-    const onScrollLeft = () => {
-        categoryListRef.current.scrollLeft -= 100;
-    };
-
-    const onScrollRight = () => {
-        categoryListRef.current.scrollLeft += 100;
-    };
-
-    return (
-        <section className="comments-block">
-            <div className="comments-block__title-block">
-                <h2 className="heading-2">Because they love us</h2>
-                <div className="comments-block__button-block">
-                    <IconButton sx={{ mr: "24px", border: "1px solid black", borderRadius: "50%" }} onClick={onScrollLeft}>
-                        <ArrowBackIcon />
-                    </IconButton>
-                    <IconButton sx={{ border: "1px solid black", borderRadius: "50%" }} onClick={onScrollRight}>
-                        <ArrowForwardIcon />
-                    </IconButton>
-                </div>
-            </div>
-            <div className="comments">
-            <div className="yelow-background"></div>
-                <div
-                    className="scroller"
-                    ref={categoryListRef}
-                    onMouseDown={handleMouseDown}
-                    onMouseLeave={handleMouseUp}
-                    onMouseUp={handleMouseUp}
-                    onMouseMove={handleMouseMove}
-                >
-                    {
-                        comments.map((comment, i) => {
-                            return <CommentCard comment={comment} key={comment.jobTitle + i} />
-                        })
-                    }
-                </div>
-            </div>
-        </section>
-    )
+    return  <Slider/>
 }
---
+++ src/components/swiper/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { comments } from '../../assets/data';
+import CommentCard from '../comment-card';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import { IconButton } from "@mui/material";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './styles.css';
+
+export default function Slider() {
+  const swiperRef = React.useRef(null);
+
+  const goNext = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
+  const goPrev = () => {
+    if (swiperRef.current && swiperRef.current.swiper) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
+  return (
+    <section className="comments-block">
+      <div className="comments-block__title-block">
+        <h2 className="heading-2">Because they love us</h2>
+        <div className="comments-block__button-block">
+          <IconButton sx={{ mr: "24px", border: "1px solid black", borderRadius: "50%" }} onClick={goPrev}>
+            <ArrowBackIcon />
+          </IconButton>
+          <IconButton sx={{ border: "1px solid black", borderRadius: "50%" }} onClick={goNext}>
+            <ArrowForwardIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="comments">
+        <div className="yelow-background"></div>
+        <Swiper
+          ref={swiperRef}
+          slidesPerView={1}
+          spaceBetween={50}
+          navigation={{ 
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+            1200: {
+              slidesPerView: 3,
+              spaceBetween: 50,
+            },
+            1600: {
+                slidesPerView: 4,
+                spaceBetween: 50,
+              },
+            1670: {
+              slidesPerView: 5,
+              spaceBetween: 200,
+            }
+          }}
+          className="mySwiper"
+        >
+          {
+            comments.map((comment, i) => {
+              return <SwiperSlide key={comment.jobTitle + i}><CommentCard comment={comment} /></SwiperSlide>
+            })
+          }
+        </Swiper>
+      </div>
+    </section>
+  );
+}
