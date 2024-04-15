882dfcdc3616315149b689b6bbde265937563eba
MkhitaryanMartin <1992Martin@mail.ru>
2024-04-15 15:10:36 +0400

    loop none

--- src/components/swiper/index.js
+++ src/components/swiper/index.js
@@ -44,7 +44,6 @@ export default function Slider() {
           ref={swiperRef}
           slidesPerView={1}
           spaceBetween={50}
-          loop={true}
           navigation={{ 
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev'
